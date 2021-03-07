107fba5837e7d8bc99b9eb6d6395bcb536c23759
Apte <manasi.apte@hp.com>
2021-03-07 23:51:11 +0530

    Fixed Horse implementation Added test case assertions.

--- src/test/java/com/test/project/gameofchess/TestRook.java
+++ src/test/java/com/test/project/gameofchess/TestRook.java
@@ -36,6 +36,24 @@ public class TestRook extends TestCase {
     	ChessPosition chessInput = ChessPosition.populateChessInput("B5");
     	ChessPiece piece = ChessPieceFactory.getChessPieceForType("Rook");
     	String rookMoves = piece.getPossibleMoves(chessInput);
-    	System.out.println(rookMoves);
+    	assertTrue(rookMoves.contains("A5"));
+    	assertTrue(rookMoves.contains("C5"));
+    	assertTrue(rookMoves.contains("D5"));
+    	assertTrue(rookMoves.contains("E5"));
+    	assertTrue(rookMoves.contains("F5"));
+    	assertTrue(rookMoves.contains("G5"));
+    	assertTrue(rookMoves.contains("H5"));
+    	
+    	assertTrue(rookMoves.contains("B4"));
+    	assertTrue(rookMoves.contains("B3"));
+    	assertTrue(rookMoves.contains("B2"));
+    	assertTrue(rookMoves.contains("B1"));
+    	
+    	assertTrue(rookMoves.contains("B6"));
+    	assertTrue(rookMoves.contains("B7"));
+    	assertTrue(rookMoves.contains("B8"));
+    	
+    	String[] splits = rookMoves.split(",");
+    	assertEquals(splits.length, 14);
     }
 }
